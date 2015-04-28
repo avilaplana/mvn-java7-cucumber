f2b3a7076418d5fcffc8f16801fca232ff9001bf
alvarovilaplanagarcia <avilaplana@equalexperts.com>
2015-04-28 23:00:27 +0100

    template for mvn java 7 and cucumber

---
+++ src/test/java/skeleton/Stepdefs.java
@@ -0,0 +1,21 @@
+package skeleton;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Stepdefs {
+
+    @Given("^I prepare a template$")
+    public void i_prepare_a_template() throws Throwable {
+    }
+
+    @When("^I run the template$")
+    public void i_run_the_template() throws Throwable {
+    }
+
+    @Then("^I have configured the project$")
+    public void i_have_configured_the_project() throws Throwable {
+
+    }
+}
---
+++ src/test/resources/skeleton/some_test.feature
@@ -0,0 +1,6 @@
+Feature: Create a template
+
+  Scenario: Run a simple scenario
+    Given I prepare a template
+    When I run the template
+    Then I have configured the project
